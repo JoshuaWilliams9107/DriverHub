a0377125368c3f72e47318ac73fd3a2869511c34
Joshua Williams <jjw2@clemson.edu>
2020-10-02 22:42:29 -0400

    Login username duplication checking, Various input validation, and user sessions and redirection

--- Node.JS SQL Login Prototype/server/server.js
+++ Node.JS SQL Login Prototype/server/server.js
@@ -1,5 +1,3 @@
-let username;
-let password;
 var mysql = require('mysql');
 var fs = require('fs');
 
@@ -18,11 +16,22 @@ con.connect(function(err) {
 });
 
 const express = require('express')
-
+var request = require("request")
 const app = express()
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
 const port = 3000
 var session = require("express-session");
-app.use(session({resave: true, saveUninitialized:true, secret: "***REMOVED***"}));
+app.use(session({
+  resave: true,
+  rolling: true,
+  saveUninitialized:true,
+  secret: "***REMOVED***",
+  cookie: {
+    expires: 60000 * 5
+  }
+  })
+   );
 app.use(express.urlencoded({
   extended: true
 }))
@@ -31,87 +40,122 @@ app.set('views', __dirname + "/../documentRoot");
 app.engine('html', require('ejs').renderFile);
 app.set('viewengine', "ejs");
 
-app.post('/submit-form', (req, res) => {
+app.post('/submit-form', async(req, res) => {
   if(!req.session.submitNumber){
     req.session.submitNumber = 0;
   }
-  req.session.submitNumber +=1;
+  console.log(req.session.submitNumber);
   if(req.session.submitNumber < 5){
-  let username = req.body.username;
-  let password = req.body.password;
-
-  let sql = "SELECT * FROM User WHERE Username= \"" + username + "\"";
-
-  con.query(sql, function (err, result) {
-   if (err) throw err;
-   if(result.length != 0){
-    //res.write('<h1>I did find that in the database<h1>');
-    if(result[0].User_Type == "Driver"){
-        res.render("driverpage.html")
+    let username = req.body.username;
+    let password = req.body.password;
+    let userType = await getUserType(username,password);
+    if(userType === false){
+      let login = encodeURIComponent("true");
+      req.session.submitNumber +=1;
+      res.redirect("/login?failedLogin=" + login)
+      return;
+    }else
+    req.session.username = username;
+    req.session.userID = userType[0].idUser;
+      if(userType[0].User_Type == "Driver"){
+        return res.redirect("/driver");
+      }else if(userType[0].User_Type == "Admin"){
+        return res.redirect("/admin");
+      }else if(userType[0].User_Type == "Sponsor"){
+        return res.redirect("/sponsor");
+      }
+    }else{
+      res.send("Too many login attempts, please try again later");
     }
-    else if(result[0].User_Type == "Admin"){
-      res.render("adminpage.html")
-    }
-    else if(result[0].User_Type == "Sponsor"){
-      res.render("sponsorpage.html")
-    }
-    res.end();
-  }else{
-    let login = encodeURIComponent("true");
-    res.redirect("/?failedLogin=" + login)
-    //res.render("index.ejs", {
-    //  test: 'Incorrect Password'
-    //});
-  }
-  });
-  }else{
-    res.send("Too many attempts to log in have been made, please try again later");
-  }
-
   //if(result[0].username == user){
   //  console.log(username);
   //  console.log(password);
   //}
 })
-/*
-function getInfo(data, callback){
-  let sql2 = `SELECT * FROM User where Username= "${req.body.create_username}"`;
-  con.query(sql2, function (err, result) {
-   let returnTag = false;
+function getUserType(username,password){
+  let sql = `SELECT * FROM User WHERE Username= "${username}" AND Password= "${password}"`;
+  return new Promise((resolve,reject) => {con.query(sql,(err, result) => {
+    if (err){
+      throw err;
+    }if(result.length != 0){
+     return err ? reject(err) : resolve(result);
+   }else{
+    return err ? reject(err) : resolve(false);
+   }
+  });
+  });
+}
+function getUnique(username){
+  let sql2 = `SELECT * FROM User where Username= "${username}"`;
+  return new Promise((resolve,reject) => {con.query(sql2,(err, result) => {
+   let isUnique = false;
    if (err){
     throw err;
     }else{
         if(result.length > 0){
-          console.log("This is not unique");
-          con.returnTag = true;
+          isUnique = false;
         }else{
-          console.log("This is unique");
+          isUnique = true;
         }
     }
-    return callback(returnTag);
+    return err ? reject(err) : resolve(result.length);
+  });
   });
 }
-*/
-app.post('/submit-form-signup', (req, res) => {
-  let username = req.body.create_username;
-  username = username.toLowerCase();
-  let sql = `INSERT INTO User (Email,First_Name,Last_Name,Username,Password,User_Type) VALUES (
-  \"${req.body.email}\",\"${req.body.first_name}\",\"${req.body.last_name}\",
-  \"${username}\",\"${req.body.create_password}\",\"Driver\")`;
+app.post('/submit-form-signup', async(req, res) => {
+  try {  
+    let username = req.body.create_username;
+    username = username.toLowerCase();
+    let numberOfExistingUsers = await getUnique(username);
+    console.log(numberOfExistingUsers);
+    if(numberOfExistingUsers > 0){
+      let login = encodeURIComponent("true");
+      res.redirect("/signup?userExists=" + login)
+      return;
+    }
+    let sql = `INSERT INTO User (Email,First_Name,Last_Name,Username,Password,User_Type) VALUES (
+    \"${req.body.email}\",\"${req.body.first_name}\",\"${req.body.last_name}\",
+    \"${username}\",\"${req.body.create_password}\",\"Driver\")`;
 
-  con.query(sql, function (err, result) {
-   if (err){
-    throw err;
-   }else{
-   	res.send("That has been put into the database")
-   }
-   });
+    con.query(sql, function (err, result) {
+    if (err){
+      throw err;
+    }else{
+      res.redirect("/login")
+    }
+    });
+  }catch (e) {
+    res.end(e.message || e.toString());
+  }
 })
 
-app.get('/', function(req, res){
+app.get('/login', function(req, res){
     res.render('index.ejs',{
       test: req.query.failedLogin
     });
+});
+app.get('/signup', function(req, res){
+  res.render('signup.ejs',{
+    userExists: req.query.userExists
+  });
+});
+app.get('/admin', function(req, res){
+  res.render('adminpage.ejs',{
+    username: req.session.username,
+    userID: req.session.userID
+  });
+});
+app.get('/driver', function(req, res){
+  res.render('driverpage.ejs',{
+    username: req.session.username,
+    userID: req.session.userID
+  });
+});
+app.get('/sponsor', function(req, res){
+  res.render('sponsorpage.ejs',{
+    username: req.session.username,
+    userID: req.session.userID
+  });
 });
 
 
@@ -123,4 +167,3 @@ app.get('/', function(req, res){
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
